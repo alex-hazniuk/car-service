15c7bda9b5cd42b62b7187429ffdbbe212bbb4ad
Alex Hazniuk <ohazniuk@gmail.com>
2023-08-06 13:25:30 +0300

    added test package

---
+++ src/test/java/org/example/service/RepairerServiceImplTest.java
@@ -0,0 +1,36 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepairerServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void changeStatus() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void sortedByName() {
+    }
+
+    @Test
+    void sortedByStatus() {
+    }
+}
