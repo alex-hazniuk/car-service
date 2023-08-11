eedfee4d51d0b51d503cd2157153c6aea35fdea1
ankoo0 <default@email.com>
2023-08-11 14:12:13 +0300

    Embedded tomcat has been added to project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import org.apache.catalina.LifecycleException;
 import org.example.management.menu.Builder;
 import org.example.management.menu.MenuController;
 import org.example.management.menu.Navigator;
 
+import javax.servlet.ServletException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ServletException, LifecycleException {
+        TomcatLauncher.launch("8080");
         Navigator navigator = new Navigator();
         Builder builder = new Builder();
 
---
+++ src/main/java/org/example/TomcatLauncher.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import javax.servlet.ServletException;
+import java.io.File;
+
+public class TomcatLauncher {
+    private static final String WEBAPP_DIR_LOCATION = "src/main/webapp/";
+
+    public static void launch(String webPort) throws ServletException, LifecycleException {
+
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.parseInt(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(WEBAPP_DIR_LOCATION).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + WEBAPP_DIR_LOCATION).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
